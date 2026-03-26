3e638a7d77c907a2386898710566b99c26f8ccb7
philip wang <philipchasewang@gmail.com>
2026-03-26 17:51:21 -0400

    Add files via upload

---
+++ hw_4_transmitted_light.py
@@ -0,0 +1,41 @@
+""" this program ultimately calculates and then outputs the transmitted light intensity through the 
+multilayer system shown in the homework by accounting for reflection losses at the interfaces and absorption within each layer 
+using Beer-Lambert law
+"""
+
+import math
+
+# initial intensity
+I0 = 10.0
+I = I0
+
+# refractive indices (air included)
+n = [1.0, 1.489, 1.333, 1.460, 1.489, 1.0]
+
+# absorption coefficients (m^-1)
+alpha = [0.132, 0.097, 0.132, 0.132]
+
+# thicknesses (m)
+t = [0.002, 0.01, 0.002, 0.02]
+
+# reflection function
+def reflection(n1, n2):
+    return ((n1 - n2) / (n1 + n2))**2
+
+# loop through each of the materials
+for i in range(len(alpha)):
+    # reflection at interface
+    R = reflection(n[i], n[i+1])
+    T = 1 - R
+    I = I * T
+
+    # absorption through material
+    I = I * math.exp(-alpha[i] * t[i])
+
+# final interface
+R_final = reflection(n[-2], n[-1])
+I = I * (1 - R_final)
+
+# print final light transmittion output
+print("The final transmitted intensity is ", I, "W/m^2")
+print("The final percent transmitted is ", (I / I0) * 100, "%")
